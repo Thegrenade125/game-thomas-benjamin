e0a23281f89b755c702f3cef1349c10e2a57f810
ErrorNoUser <bplaag78@gmail.com>
2026-05-02 04:05:43 +0000

    Input and function creation

--- the-game.c
+++ the-game.c
@@ -1,14 +1,43 @@
-clear();
 
-for(int i = 0; i < WIDTH; i++) {
-    mvprintw(0, i, "#");
-    mvprintw(HEIGHT - 1, i, "#");
+#include <ncurses.h>
+#include <stdlib.h>
+
+
+
+void handle_input(int *x, int *y) {
+    int ch = getch();
+    switch (ch) {
+            case KEY_UP:    (*y)--; 
+            break;
+            case KEY_DOWN:  (*y)++; 
+            break;
+            case KEY_LEFT:  (*x)--; 
+            break;
+            case KEY_RIGHT: (*x)++; 
+            break;
+    }
 }
-for(int i = 0; i< HEIGHT; i++){
-    mvprintw(i, 0, "#");
-    mvprintw(i, WIDTH - 1, "#")
+
+void print(int x, int y){
+    clear();
+
+    for(int i = 0; i < WIDTH; i++) {
+        mvprintw(0, i, "#");
+        mvprintw(HEIGHT - 1, i, "#");
+}
+    for(int i = 0; i< HEIGHT; i++){
+        mvprintw(i, 0, "#");
+        mvprintw(i, WIDTH - 1, "#")
 }
 
-mvprintw(y, x, "0");
+        mvprintw(y, x, "0");
 
-refresh();
+    refresh();
+}
+
+int main (){
+
+
+
+    return 0; 
+}
