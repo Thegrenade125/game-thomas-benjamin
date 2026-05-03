a23b32a4a9fe1fc4d35f0c50010d20c182d8bc82
Thegrenade125 <153784728+Thegrenade125@users.noreply.github.com>
2026-05-03 11:41:34 -0400

    Initialize game coordinates and main loop

--- the-game.c
+++ the-game.c
@@ -2,7 +2,8 @@
 #include <ncurses.h>
 #include <stdlib.h>
 
-
+#define WIDTH 20
+#define HEIGHT 20
 
 void handle_input(int *x, int *y) {
     int ch = getch();
@@ -24,20 +25,33 @@ void print(int x, int y){
     for(int i = 0; i < WIDTH; i++) {
         mvprintw(0, i, "#");
         mvprintw(HEIGHT - 1, i, "#");
-}
+    }
     for(int i = 0; i< HEIGHT; i++){
         mvprintw(i, 0, "#");
         mvprintw(i, WIDTH - 1, "#")
-}
+    }
 
-        mvprintw(y, x, "0");
+    mvprintw(y, x, "0");
 
     refresh();
 }
 
 int main (){
+    int x = WIDTH / 2;
+    int y = HEIGHT / 2;
 
+    intiscr();
+    neocho();
+    curs_set(FALSE);
+    keypad(stdscr, TRUE);
+    nodelay(stdscr, TRUE);
 
+    while(1){
+        print(x, y);
+        handle_input(&x, &y);
+        napms(100);
+    }
 
+    endwin();
     return 0; 
 }
