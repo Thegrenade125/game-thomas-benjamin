1bc28b57609bee9df14af8d214a8f60f0be98cc0
ErrorNoUser <bplaag78@gmail.com>
2026-05-05 02:42:23 -0400

    Added start menu and game over menu
    Added the start and game over menus. Also tweaked the location of loops in while loop to check correctly.

--- the-game.c
+++ the-game.c
@@ -2,6 +2,7 @@
 #include <ncurses.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 #define WIDTH 20
 #define HEIGHT 20
@@ -32,7 +33,7 @@ void handle_input(int *x, int *y) {
     }
 }
 
-void print(int x, int y){
+void draw(int x, int y){
     clear();
 
     for(int i = 0; i < WIDTH; i++) {
@@ -52,7 +53,42 @@ void print(int x, int y){
 
     mvprintw(food_y, food_x, "*");
 
+    mvprintw(HEIGHT + 1, 0, "Score: %d", snake_length - 1);
+    
     refresh();
+}
+
+void show_start_screen(){
+    clear();
+    mvprintw(HEIGHT / 2 - 2, WIDTH / 2 - 4, "S N A K E");
+    mvprintw(HEIGHT / 2,     WIDTH / 2 - 7, "Press any key to start");
+    refresh();
+    nodelay(stdscr, FALSE);
+    getch();
+    nodelay(stdscr, TRUE);
+}
+
+int show_game_over_screen(int score){
+    clear();
+    mvprintw(HEIGHT / 2 - 2, WIDTH / 2 - 4, "GAME OVER");
+    mvprintw(HEIGHT / 2,     WIDTH / 2 - 5, "Score: %d", score);
+    mvprintw(HEIGHT / 2 + 2, WIDTH / 2 - 9, "Press R to play again or Q to quit");
+    refresh();
+    nodelay(stdscr, FALSE);
+    while(1){
+        int ch = getch();
+        if(ch == 'r' || ch == 'R') { nodelay(stdscr, TRUE); return 1; }
+        if(ch == 'q' || ch == 'Q') { nodelay(stdscr, TRUE); return 0; }
+    }
+}
+
+void reset_game(int *x, int *y, int *dx, int *dy){
+    *x = WIDTH / 2;
+    *y = HEIGHT / 2;
+    snake_length = 1;
+    snake_x[0] = *x;
+    snake_y[0] = *y;
+    spawn_food();
 }
 
 int main(){
@@ -75,27 +111,39 @@ int main(){
             snake_x[i] = snake_x[i-1];
             snake_y[i] = snake_y[i-1];
         }
+        
+        snake_x[0] = x;
+        snake_y[0] = y;
+
+        //ends game after hitting the wall (still needs to check for hitting itself)
+        if (x <= 0 || x >= WIDTH-1 || y <= 0 || y >= HEIGHT-1){
+            game_over = 1;
+            break;
+        }
+
         //spawns food after increases length
         if (x == food_x && y == food_y) {
             snake_length++;
             spawn_food();
         }
         
-        snake_x[0] = x;
-        snake_y[0] = y;
-
-        //ends game after hitting the wall (still needs to check for hitting itself)
-        if (x <= 0 || x >= WIDTH-1 || y <= 0 || y >= HEIGHT-1)
-            break;
         for(int i = 1; i < snake_length; i++){
             if(x == snake_x[i] && y == snake_y[i]){
+                game_over = 1;
                 break;
             }
         }
-        print(x, y);
-        napms(100); 
+        if(!game_over){
+            draw(x, y);
+            napms(150); 
+        }
+
+        if(!show_game_over_screen(snake_length - 1))
+            break;
     }
 
+    
+    
     endwin();
     return 0; 
 }
