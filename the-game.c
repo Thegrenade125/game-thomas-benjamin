4bf189fe1f0c187b4124cc0d1105f423788c9140
ErrorNoUser <bplaag78@gmail.com>
2026-05-04 22:46:27 -0400

    Fixed small errors and added snake movements and end game

--- the-game.c
+++ the-game.c
@@ -9,9 +9,13 @@
 int food_x;
 int food_y;
 
+int snake_x[100];
+int snake_y[100];
+int snake_length = 1;
+
 void spawn_food(){
     food_x = (rand() % (WIDTH - 2) + 1);
-    food_y = (rand() % (HEIGHT - 2)) + 1);
+    food_y = (rand() % (HEIGHT - 2) + 1);
 }
 
 void handle_input(int *x, int *y) {
@@ -40,9 +44,13 @@ void print(int x, int y){
         mvprintw(i, WIDTH - 1, "#");
     }
 
+    for(int i = 1; i < snake_length; i++) {
+        mvprintw(snake_y[i], snake_x[i], "o");
+    }
+
     mvprintw(y, x, "0");
 
-    mvprintw(food_y, ood_x, "*");
+    mvprintw(food_y, food_x, "*");
 
     refresh();
 }
@@ -54,16 +62,39 @@ int main(){
     srand(time(NULL));
     spawn_food();
 
-    intiscr();
-    neocho();
+    initscr();
+    noecho();
     curs_set(FALSE);
     keypad(stdscr, TRUE);
     nodelay(stdscr, TRUE);
 
     while(1){
-        print(x, y);
         handle_input(&x, &y);
+        //moves the body with the head
+        for(int i = snake_length - 1; i > 0; i--) {
+            snake_x[i] = snake_x[i-1];
+            snake_y[i] = snake_y[i-1];
+        }
+        //spawns food after increases length
+        if (x == food_x && y == food_y) {
+            snake_length++;
+            spawn_food();
+        }
+        
+        snake_x[0] = x;
+        snake_y[0] = y;
+        
+        print(x, y);
+
+        //ends game after hitting the wall (still needs to check for hitting itself)
+        if (x <= 0 || x >= WIDTH-1 || y <= 0 || y >= HEIGHT-1)
+            break;
+    }
+        
         napms(100);
+
+
+        
     }
 
     endwin();
